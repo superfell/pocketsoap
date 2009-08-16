61c246106442e58ac999ebbc8041dfd764dcbce2
fellforce <fellforce@77a55350-a47a-11dd-988f-d3ef9af6d102>
2009-08-16 19:35:24 +0000

    v1.5.5 release branch

--- pocketsoap/docs/makep.cmd
+++
@@ -1,9 +0,0 @@
-rem @echo off
-call settings.cmd
-del proxydocs\*.* /q
-if exist DocGen.log del DocGen.log
-
-wizrun.exe /q %PATHTODOCGEN%\docgenwizard.gxw projectName=proxydocs projectDirectory="%PATHTOPS%\docs\proxydocs" idlFile="%PATHTOPS%\psProxy_Core\psProxy.idl" xmlFile="%PATHTOPS%\docs\proxy.xml"
-
-
-if exist DocGen.log type DocGen.log
--- pocketsoap/pSOAP_W32/_BldNum.h
+++
@@ -1,8 +0,0 @@
-// Project: C:\soap_src\SimonFell\pocketsoap\pSOAP_W32\pSOAP32.dsp.  DO NOT MODIFY THIS FILE BY HAND.
-//
-#define BUILDNUM_ENABLED (1)
-#define MODIFY_VERSIONINFO (1)
-#define FILE_VERSION0 (1)
-#define FILE_VERSION1 (4)
-#define FILE_VERSION2 (3)
-#define BUILD_NUMBER (3548)
